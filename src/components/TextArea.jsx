d333ebf31a8f57f7c0a84b27d28bfcacfa0d6b75
AlexSimpson97 <alexandersimpson65.ts@gmail.com>
2023-12-10 18:25:48 +0000

    Added new word filters

--- src/components/TextArea.jsx
+++ src/components/TextArea.jsx
@@ -20,6 +20,10 @@ export default function TextArea({userText, SetUserText, warningText, setWarning
             setWarningText("No fucks allowed, straight to horny jail!");
             moddedText = moddedText.replace("fuck", "JAIL!");
         }
+        } else if (moddedText.includes("Fuck")) {
+            setWarningText("No fucks allowed, straight to horny jail!");
+            moddedText = moddedText.replace("fuck", "JAIL!");
+        }
 
         SetUserText(moddedText);
     }
