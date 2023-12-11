c728d1f31d40d00b866d0a7dee0835bc804d8857
AlexSimpson97 <alexandersimpson65.ts@gmail.com>
2023-12-11 14:25:39 +0000

    Fix for filter edge case

--- src/components/TextArea.jsx
+++ src/components/TextArea.jsx
@@ -19,9 +19,9 @@ export default function TextArea({userText, SetUserText, warningText, setWarning
             { regex: /nigger/gi, warning: "Not on our watch :)", replacement: "Nah" },
             { regex: /\bcoon\b/gi, warning: "Consider this, instead", replacement: "Uncle Tom" },
             { regex: /\bhoes\b/gi, warning: "Come on now, be nice", replacement: "heroes" },
-            { regex: /hoe/gi, warning: "Come on now, be nice", replacement: "hero" },
+            { regex: /\bhoe\b/gi, warning: "Come on now, be nice", replacement: "hero" },
             { regex: /\bhos\b/gi, warning: "Come on now, be nice", replacement: "heroes" },
-            { regex: /\bho\b/gi, warning: "Come on now, be nice", replacement: "hero" },
+            { regex: /\bho\b/gi, warning: "Come on now, be nice", replacement: "hero" }, 
         ];
     
         for (let i = 0; i < cases.length; i++) {
